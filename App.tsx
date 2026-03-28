963430cdf96fb0de927e70b120a3532b7c05c12b
Stéphane Prud’homme <stephaneprudhomme@ee:4f:7a:dd:af:f9.home>
2026-03-28 10:59:53 -0400

    Update copyright footer wording

--- App.tsx
+++ App.tsx
@@ -4592,7 +4592,7 @@ const App: React.FC = () => {
             <footer className="mt-10 rounded-2xl border border-slate-200 bg-white px-6 py-4 text-xs text-slate-500 shadow-sm">
               <p>Ⓒ Stéphane Prud&apos;homme, Tous droits réservés 2026</p>
               <p className="mt-1">
-                Partage, reproduction, utilisation du matériel avec approbation préliminaire et pour des fins éducatives seulement
+                Partage, reproduction, utilisation du matériel avec approbation préliminaire et à des fins éducatives et académiques seulement.
               </p>
             </footer>
           </>
